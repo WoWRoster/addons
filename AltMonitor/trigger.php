23aab5af94f3ff7a624159f9f725d4a2cef6b9c8
pleegwat <pleegwat@b904fcd1-60ed-da51-8dba-9922f2c0e645>
2006-12-24 11:57:31 +0000

    Added current development version of AltMonitor

---
+++ AltMonitor/trigger.php
@@ -0,0 +1,49 @@
+<?php
+/******************************
+ * WoWRoster.net  Roster
+ * Copyright 2002-2006
+ * Licensed under the Creative Commons
+ * "Attribution-NonCommercial-ShareAlike 2.5" license
+ *
+ * Short summary
+ *  http://creativecommons.org/licenses/by-nc-sa/2.5/
+ *
+ * Full license information
+ *  http://creativecommons.org/licenses/by-nc-sa/2.5/legalcode
+ ******************************/
+
+if ( !defined('ROSTER_INSTALLED') )
+{
+    exit('Detected invalid access to this file!');
+}
+
+/*
+	Start the following scripts when "update.php" is called
+
+	Available variables
+		- $wowdb       = roster's db layer
+		- $member_id   = character id from the database ( ex. 24 )
+		- $member_name = character's name ( ex. 'Jonny Grey' )
+		- $roster_conf = The entire roster config array
+		- $mode        = when you want to run the trigger
+			= 'char'  - during a character update
+			= 'guild' - during a guild update
+
+	You may need to do some fancy coding if you need more variables
+
+	Just print any needed output
+
+*/
+
+// We're only updating on guild updates.
+if( $mode == 'guild' )
+{
+	$retval = include($addonDir.'update.php');
+	
+	if ($retval != 1) $messages .= "AltMonitor error: ".$retval."<br/>\n";
+	
+	// echo messages for roster's update.php
+	echo $messages;
+}
+
+?>
