a85f4bc71aeb998013aa44fe4bf3da411714e2a0
titan99 <titan99@b904fcd1-60ed-da51-8dba-9922f2c0e645>
2007-02-15 14:02:46 +0000

    firts version file

---
+++ key_bc/localization.php
@@ -0,0 +1,388 @@
+<?php
+/******************************
+ * WoWRoster.net  Roster
+ * Copyright 2002-2006
+ * Licensed under the Creative Commons
+ * "Attribution-NonCommercial-ShareAlike 2.5" license
+ *
+ * Short summary
+ *  http://creativecommons.org/licenses/by-nc-sa/2.5/
+ *
+ * Full license information
+ *  http://creativecommons.org/licenses/by-nc-sa/2.5/legalcode
+ * -----------------------------
+ *
+ * $Id: localization.php 19 2006-12-27 06:25:26Z zanix $
+ *
+ ******************************/
+
+$wordings['addoncredits']['Key bc v0.1'] = array(
+	array(	"name"=>	"Titan99",
+		"info"=>	"look a key bc"),
+);
+
+// ALLIANCE KEYS
+$wordings['frFR']['kbc_title_addon'] = 'Clef BC';
+
+$inst_keys_bc['frFR']['A'] = array(
+	'SL' => array( 'Key-Only', 'SL' =>
+			'Clé du labyrinthe des ombres|57267'
+		),
+	'SH' => array( 'Quests', 'SH' =>
+			'Clé des Salles Brisées|56320',
+			'L\\\'entrée dans la citadelle|',
+			'Le grand maître Dumphry|',
+			'La demande de Dumphry|',
+			'Plus chaud que l\\\'enfer|'
+		),
+	'Arcatraz' => array( 'Quests', 'Arcatraz' =>
+			'Clé de l\\\'Arcatraz|58724',
+			'Point de triangulation numéro un|',
+			'Point de triangulation numéro deux|',
+			'Le triangle est triangulé|',
+			'Livraison spéciale à Shattrath|',
+			'Comment pénétrer dans l\\\'Arcatraz|'
+		),
+	'TK' => array( 'Quests', 'TK' =>
+			'La clé de la Tempête|60250',
+			'L\\\'épreuve des naaru : Miséricorde|',
+			'L\\\'épreuve des naaru : Force|',
+			'L\\\'épreuve des naaru : Ténacité|',
+			'Livraison spéciale à Shattrath|',
+			'La clé de la Tempête|'
+		),
+	'Kara' => array( 'Quests', 'Kara' =>
+			'La clé du maître|58887',
+			'Troubles arcaniques|',
+			'L\\\'agitation des sans-repos|',
+			'Un envoyé de Dalaran||MS',
+			'Khadgar|',
+			'L\\\'entrée de Karazhan|',
+			'Le deuxième et le troisième fragment|',
+			'Le toucher du maître|',
+			'Retour vers Khadgar|',
+		),
+	'HA' => array( 'Key-Only', 'HA' =>
+			'Clé Auchenaï|55216'
+		),
+	'HHC' => array( 'Key-Only', 'HHC' =>
+			'Clé en flammes forgées|55262'
+		),
+	'HCR' => array( 'Key-Only', 'HCR' =>
+			'Clé du réservoir|55139'
+		),
+	'HTK' => array( 'Key-Only', 'HTK' =>
+			'Clé dimensionnelle|55231'
+		),
+	'HCoT' => array( 'Key-Only', 'HCoT' =>
+			'Clé du Temps|54481'
+		),
+);
+
+
+// HORDE KEYS
+$inst_keys_bc['frFR']['H'] = array(
+	'SL' => array( 'Key-Only', 'SL' =>
+			'Clé du labyrinthe des ombres|57267'
+		),
+	'SH' => array( 'Quests', 'SH' =>
+			'Clé des Salles Brisées|56320',
+			'L\\\'entrée dans la citadelle|',
+			'Le grand maître Rohok|',
+			'La demande de Rohok|',
+			'Plus chaud que l\\\'enfer|'
+		),
+	'Arcatraz' => array( 'Quests', 'Arcatraz' =>
+			'Clé de l\\\'Arcatraz|58724',
+			'Point de triangulation numéro un|',
+			'Point de triangulation numéro deux|',
+			'Le triangle est triangulé|',
+			'Livraison spéciale à Shattrath|',
+			'Comment pénétrer dans l\\\'Arcatraz|'
+		),
+	'TK' => array( 'Quests', 'TK' =>
+			'La clé de la Tempête|60250',
+			'L\\\'épreuve des naaru : Miséricorde|',
+			'L\\\'épreuve des naaru : Force|',
+			'L\\\'épreuve des naaru : Ténacité|',
+			'Livraison spéciale à Shattrath|',
+			'La clé de la Tempête|'
+		),
+	'Kara' => array( 'Quests', 'Kara' =>
+			'La clé du maître|58887',
+			'Troubles arcaniques|',
+			'L\\\'agitation des sans-repos|',
+			'Un envoyé de Dalaran||MS',
+			'Khadgar|',
+			'L\\\'entrée de Karazhan|',
+			'Le deuxième et le troisième fragment|',
+			'Le toucher du maître|',
+			'Retour vers Khadgar|',
+		),
+	'HA' => array( 'Key-Only', 'HA' =>
+			'Clé Auchenaï|55216'
+		),
+	'HHC' => array( 'Key-Only', 'HHC' =>
+			'Clé en flammes forgées|55262'
+		),
+	'HCR' => array( 'Key-Only', 'HCR' =>
+			'Clé du réservoir|55139'
+		),
+	'HTK' => array( 'Key-Only', 'HTK' =>
+			'Clé dimensionnelle|55231'
+		),
+	'HCoT' => array( 'Key-Only', 'HCoT' =>
+			'Clé du Temps|54481'
+		),
+);
+
+/*********************DE*************************************************/
+
+$inst_keys_bc['deDE']['A'] = array(
+	'SL' => array( 'Key-Only', 'SL' =>
+			'SchlÃ¼ssel des Schattenlabyrinths|57267'
+		),
+	'ZH' => array( 'Quests', 'ZH' =>
+			'SchlÃ¼ssel der zerschmetterten Hallen|56320',
+			'Zugang zur Zitadelle|',
+			'GroÃŸmeister Dumphry|',
+			'Dumphrys Bitte|',
+			'HeiÃŸer als die HÃ¶lle|'
+		),
+	'Arkatraz' => array( 'Quests', 'Arkatraz' =>
+			'SchlÃ¼ssel zu Arkatraz|58724',
+			'Kristallsammlung des Konsortiums|',
+			'Ein HÃ¼gel voll Astraler|',
+			'SphÃ¤renrÃ¤uber Nesaad|',
+			'Bitte um UnterstÃ¼tzung|',
+			'RechtmÃ¤ÃŸiger Besitz|',
+			'Eine Audienz beim Prinzen|',
+			'Triangulationspunkt Eins|',
+			'Triangulationspunkt Zwei|',
+			'Das volle Dreieck|',
+			'Sonderlieferung nach Shattrath|',
+			'Wie man in Arkatraz einbricht|'
+		),
+	'Karazhan' => array( 'Quests', 'Karazhan' => 
+			'Der SchlÃ¼ssel des Meisters|58887',
+			'Arkane StÃ¶rungen|',
+			'Rastlose AktivitÃ¤t|',
+			'Dalarankontaktpersonen||MS',
+			'Khadgar|',
+			'Nach Karazhan|',
+			'Das zweite und das dritte Fragment|',
+			'Die BerÃ¼hrung des Meisters|',
+			'RÃ¼ckkehr zu Khadgar|',
+		),
+	'Auge' => array( 'Quests', 'Auge' =>
+			'SchlÃ¼ssel der StÃ¼rme|58724',
+			'Die PrÃ¼fung der Naaru: Erbarmen|',
+			'Die PrÃ¼fung der Naaru: StÃ¤rke|',
+			'Die PrÃ¼fung der Naaru: ZuverlÃ¤ssigkeit|',
+			'Sonderlieferung nach Shattrath|',
+			'Der SchlÃ¼ssel der StÃ¼rme|' 
+		),
+	'hA' => array( 'Key-Only', 'hA' =>
+			'SchlÃ¼ssel der Auchenai|55216'
+		 ),
+	'hHZ' => array( 'Key-Only', 'hHZ' => 
+			'Flammengeschmiedeter SchlÃ¼ssel|55262'
+		), 
+	'hEK' => array( 'Key-Only', 'hEK' =>
+			'SchlÃ¼ssel des Kessels|55139'
+		),
+	'hFdS' => array( 'Key-Only', 'hFdS' =>
+			'Warpgeschmiedeter SchlÃ¼ssel|55231'
+		),
+	'hHdZ' => array( 'Key-Only', 'hHdZ' =>
+			'SchlÃ¼ssel der Zeit|54481'
+		 ),
+);
+
+
+// HORDE KEYS
+$inst_keys_bc['deDE']['H'] = array(
+	'SL' => array( 'Key-Only', 'SL' =>
+			'SchlÃ¼ssel des Schattenlabyrinths|57267'
+		),
+	'ZH' => array( 'Quests', 'ZH' =>
+			'SchlÃ¼ssel der zerschmetterten Hallen|56320',
+			'Zugang zur Zitadelle|',
+			'GroÃŸmeister Dumphry|',
+			'Dumphrys Bitte|',
+			'HeiÃŸer als die HÃ¶lle|'
+		),
+	'Arkatraz' => array( 'Quests', 'Arkatraz' =>
+			'SchlÃ¼ssel zu Arkatraz|58724',
+			'Kristallsammlung des Konsortiums|',
+			'Ein HÃ¼gel voll Astraler|',
+			'SphÃ¤renrÃ¤uber Nesaad|',
+			'Bitte um UnterstÃ¼tzung|',
+			'RechtmÃ¤ÃŸiger Besitz|',
+			'Eine Audienz beim Prinzen|',
+			'Triangulationspunkt Eins|',
+			'Triangulationspunkt Zwei|',
+			'Das volle Dreieck|',
+			'Sonderlieferung nach Shattrath|',
+			'Wie man in Arkatraz einbricht|'
+		),
+	'Karazhan' => array( 'Quests', 'Karazhan' => 
+			'Der SchlÃ¼ssel des Meisters|58887',
+			'Arkane StÃ¶rungen|',
+			'Rastlose AktivitÃ¤t|',
+			'Dalarankontaktpersonen||MS',
+			'Khadgar|',
+			'Nach Karazhan|',
+			'Das zweite und das dritte Fragment|',
+			'Die BerÃ¼hrung des Meisters|',
+			'RÃ¼ckkehr zu Khadgar|',
+		),
+	'Auge' => array( 'Quests', 'Auge' =>
+			'SchlÃ¼ssel der StÃ¼rme|58724',
+			'Die PrÃ¼fung der Naaru: Erbarmen|',
+			'Die PrÃ¼fung der Naaru: StÃ¤rke|',
+			'Die PrÃ¼fung der Naaru: ZuverlÃ¤ssigkeit|',
+			'Sonderlieferung nach Shattrath|',
+			'Der SchlÃ¼ssel der StÃ¼rme|' 
+		),
+	'hA' => array( 'Key-Only', 'hA' =>
+			'SchlÃ¼ssel der Auchenai|55216'
+		 ),
+	'hHZ' => array( 'Key-Only', 'hHZ' => 
+			'Flammengeschmiedeter SchlÃ¼ssel|55262'
+		), 
+	'hEK' => array( 'Key-Only', 'hEK' =>
+			'SchlÃ¼ssel des Kessels|55139'
+		),
+	'hFdS' => array( 'Key-Only', 'hFdS' =>
+			'Warpgeschmiedeter SchlÃ¼ssel|55231'
+		),
+	'hHdZ' => array( 'Key-Only', 'hHdZ' =>
+			'SchlÃ¼ssel der Zeit|54481'
+		 ), 
+);
+
+
+/*********************EN*************************************************/
+
+$inst_keys_bc['enUs']['A'] = array(
+	'SH' => array( 'Quests', 'ShatteredHalls' =>
+                        'Shattered Halls Key|56320',
+                        'Entry into the Citadel|',
+                        'Grand Master Dumphry|',
+                        'Dumphry\\\'s Request|',
+                        'Hotter then Hell|'
+                ),
+        'SL' => array( 'Key-Only', 'ShadowLabyrinth' =>
+                        'Shadow Labyrinth Key|57267',
+                ),
+        'Arcatraz' => array( 'Quests', 'Arcatraz' =>
+                        'Key to the Arcatraz|58724',
+                        'Warp-Raider Nesaad|',
+                        'Request for Assistance|',
+                        'Rightful Repossession|',
+                        'An Audience with the Prince|',
+                        'Triangulation Point One|',
+                        'Triangulation Point Two|',
+                        'Full Triangle|',
+                        'Special Delivery to Shattrath City|',
+                        'How to Break Into the Arcatraz|'
+                ),
+        'TK' => array( 'Quests', 'TheEye' =>
+                        'The Tempest Key|58724',
+                        'Trial of the Naaru: Mercy|',
+                        'Trial of the Naaru: Strength|',
+                        'Trial of the Naaru: Tenacity|',
+                        'Special Delivery to Shattrath City|',
+                        'The Tempest Key|'
+                ),
+        'Kara' => array( 'Quests', 'Karazhan' =>
+                        'The Master\\\'s Key|58887',
+                        'Arcane Disturbances|',
+                        'Restless Activity|',
+                        'Contact from Dalaran||MS',
+                        'Khadgar|',
+                        'Entry Into Karazhan|',
+                        'The Second and Third Fragments|',
+                        'The Master\\\'s Touch|',
+                        'Return to Khadgar|',
+                ),
+        'HA' => array( 'Key-Only', 'HA' =>
+                        'Auchenai Key|55216'
+                ),
+        'HHC' => array( 'Key-Only', 'HHC' =>
+                        'Flamewrought Key|55262'
+                ),
+        'HCR' => array( 'Key-Only', 'HCR' =>
+                        'Reservoir Key|55139'
+                ),
+        'HTK' => array( 'Key-Only', 'HTK' =>
+                        'Warpforged Key|55231'
+                ),
+        'HCoT' => array( 'Key-Only', 'HCoT' =>
+                        'Key of Time|54481'
+                ),
+);
+
+
+// HORDE KEYS
+$inst_keys_bc['enUS']['H'] = array(
+	'SH' => array( 'Quests', 'ShatteredHalls' =>
+                        'Shattered Halls Key|56320',
+                        'Entry into the Citadel|',
+                        'Grand Master Dumphry|',
+                        'Dumphry\\\'s Request|',
+                        'Hotter then Hell|'
+                ),
+        'SL' => array( 'Key-Only', 'ShadowLabyrinth' =>
+                        'Shadow Labyrinth Key|57267',
+                ),
+        'Arcatraz' => array( 'Quests', 'Arcatraz' =>
+                        'Key to the Arcatraz|58724',
+                        'Warp-Raider Nesaad|',
+                        'Request for Assistance|',
+                        'Rightful Repossession|',
+                        'An Audience with the Prince|',
+                        'Triangulation Point One|',
+                        'Triangulation Point Two|',
+                        'Full Triangle|',
+                        'Special Delivery to Shattrath City|',
+                        'How to Break Into the Arcatraz|'
+                ),
+        'TK' => array( 'Quests', 'TheEye' =>
+                        'The Tempest Key|58724',
+                        'Trial of the Naaru: Mercy|',
+                        'Trial of the Naaru: Strength|',
+                        'Trial of the Naaru: Tenacity|',
+                        'Special Delivery to Shattrath City|',
+                        'The Tempest Key|'
+                ),
+        'Kara' => array( 'Quests', 'Karazhan' =>
+                        'The Master\\\'s Key|58887',
+                        'Arcane Disturbances|',
+                        'Restless Activity|',
+                        'Contact from Dalaran||MS',
+                        'Khadgar|',
+                        'Entry Into Karazhan|',
+                        'The Second and Third Fragments|',
+                        'The Master\\\'s Touch|',
+                        'Return to Khadgar|',
+                ),
+        'HA' => array( 'Key-Only', 'HA' =>
+                        'Auchenai Key|55216'
+                ),
+        'HHC' => array( 'Key-Only', 'HHC' =>
+                        'Flamewrought Key|55262'
+                ),
+        'HCR' => array( 'Key-Only', 'HCR' =>
+                        'Reservoir Key|55139'
+                ),
+        'HTK' => array( 'Key-Only', 'HTK' =>
+                        'Warpforged Key|55231'
+                ),
+        'HCoT' => array( 'Key-Only', 'HCoT' =>
+                        'Key of Time|54481'
+                ),
+);
+
+
