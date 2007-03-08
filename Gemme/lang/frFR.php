5e667c5779e9c911b02e21f29c65ee5ee60664fc
titan99 <titan99@b904fcd1-60ed-da51-8dba-9922f2c0e645>
2007-03-08 12:37:05 +0000

    Gems Display addons to diplay gems by color slot

---
+++ Gemme/lang/frFR.php
@@ -0,0 +1,28 @@
+<?php
+// ALLIANCE KEYS
+$wordings['frFR']['gemme_title_addon'] = 'Gemmes';
+
+$Gem_info['frFR']['Gem_title']="Gemmes pour Châsse";
+$Gem_info['frFR']['Objet']='Objet';
+$Gem_info['frFR']['Name']='Nom';
+$Gem_info['frFR']['level']='Niveau';
+$Gem_info['frFR']['compo']='Réactifs';
+$Gem_info['frFR']['crafter']='Artisants';
+
+
+//Craft Name
+$Gem_info['frFR']['Gem']="Gemmes";
+$Gem_info['frFR']['sill']="Joaillerie";
+
+//slot name
+$Gem_info['frFR']['slot']="châsse";
+
+//color
+$Gem_info['frFR']['type']['blue']="bleue";
+$Gem_info['frFR']['type']['red']="rouge";
+$Gem_info['frFR']['type']['yellow']="jaune";
+$Gem_info['frFR']['type']['meta']="méta-châsses";
+
+
+
+?>
