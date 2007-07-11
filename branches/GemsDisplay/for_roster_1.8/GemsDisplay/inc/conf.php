404767fba2cdbf6106b7f8c01384b1e41d9a6996
titan99 <titan99@b904fcd1-60ed-da51-8dba-9922f2c0e645>
2007-07-11 12:11:01 +0000

    Gems Display : - adding a branche for the dev to roster 1.8

---
+++ branches/GemsDisplay/for_roster_1.8/GemsDisplay/inc/conf.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * WoWRoster.net WoWRoster
+ *
+ * LICENSE: Licensed under the Creative Commons
+ *          "Attribution-NonCommercial-ShareAlike 2.5" license
+ *
+ * @copyright  2002-2007 WoWRoster.net
+ * @license    http://creativecommons.org/licenses/by-nc-sa/2.5   Creative Commons "Attribution-NonCommercial-ShareAlike 2.5"
+ * @version    SVN: $ v0.5 Titan99 and Tyradil $
+ * @link       http://www.wowroster.net
+ * @package    GemsDisplay
+*/
+
+$color['blue']="#0000FF";
+$color['red']="#FF0000";
+$color['yellow']="#FFFF66";
+$color['green']="#33CC33";
+$color['orange']="#FF9900";
+$color['purple']="#CC00FF";
+$color['meta']="#AAAAAA";
