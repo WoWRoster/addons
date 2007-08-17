ca9e6cd9970bfe7b064767e8595353b439fafc16
titan99 <titan99@b904fcd1-60ed-da51-8dba-9922f2c0e645>
2007-08-17 11:29:10 +0000

    Key BC : save for roster 1.7

---
+++ branches/Key_BC/roster_v1.7/Key_BC/lang/deDE.php
@@ -0,0 +1,195 @@
+<?php
+/*	tank to daddelkopp */
+/*********************DE*************************************************/
+
+$wordings['deDE']['kbc_title_addon'] = 'Schlüssel BC';
+$wordings['deDE']['Link_to_classic_keys'] = 'Link zu alten WoW Schlüsseln';
+
+
+$inst_name['SL']="Laby.";
+$inst_name['SH']="Hallen";
+$inst_name['Arcatraz']="Arkatraz";
+$inst_name['Kara']="Karazhan";
+$inst_name['HA']="H Laby.";
+$inst_name['HHC']="H Hallen";
+$inst_name['HCR']="H Kessel";
+$inst_name['HTK']="H FdS";
+$inst_name['HCoT']="H HdZ";
+$inst_name['TK']="Auge";
+
+
+$inst_keys_bc['deDE']['A'] = array(
+	'SL' => array( 'Key-Only', 'SL' =>
+			'Schlüssel des Schattenlabyrinths|57267'
+		),
+	'SH' => array( 'Quests', 'SH' =>
+			'Schlüssel der zerschmetterten Hallen|56320',
+			'Zugang zur Zitadelle|',
+			'Großmeister Dumphry|',
+			'Dumphrys Bitte|',
+			'Heißer als die Hölle|'
+		),
+	'Arcatraz' => array( 'Quests', 'Arcatraz' =>
+			'Schlüssel zu Arkatraz|58724',
+			'Kristallsammlung des Konsortiums|',
+			'Ein Hügel voll Astraler|',
+			'Sphärenräuber Nesaad|',
+			'Bitte um Unterstätzung|',
+			'Rechtmäßiger Besitz|',
+			'Eine Audienz beim Prinzen|',
+			'Triangulationspunkt Eins|',
+			'Triangulationspunkt Zwei|',
+			'Das volle Dreieck|',
+			'Sonderlieferung nach Shattrath|',
+			'Wie man in Arkatraz einbricht|'
+		),
+	'Kara' => array( 'Quests', 'Kara' =>
+			'Der Schlüssel des Meisters|58887',
+			'Arkane Störungen|',
+			'Rastlose Aktivität|',
+			'Dalarankontaktpersonen||MS',
+			'Khadgar|',
+			'Nach Karazhan|',
+			'Das zweite und das dritte Fragment|',
+			'Die Berührung des Meisters|',
+			'Rückkehr zu Khadgar|'
+		),
+	'TK' => array( 'Quests', 'TK' => 
+			'Schlüssel der Stürme|60250',
+			'Oronok Herzeleid|',
+			'Ich war schon vieles...|',
+			'Eine Lektion gelernt|',
+			'Die Litanei der Verdammnis - Wahrheit und Geschichte|',
+			'Grom\\\'tor, Sohn des Oronok|',
+			'Die Litanei der Verdammnis - Grom\\\'tors Angriff|',
+			'Die Litanei der Verdammnis - Erster Teil|',
+			'Ar\\\'tor, Sohn des Oronok|',
+			'Dämonische Kristallgefängnisse|',
+			'Lohn\\\'goron, Bogen des Herzeleid|',
+			'Die Litanei der Verdammnis - Ar\\\'tors Angriff|',
+			'Die Litanei der Verdammnis - Zweiter Teil|',
+			'Borak, Sohn des Oronok|',
+			'Von Distelköpfen und Eiern...|',
+			'Ein Bündel von Blutdisteln|',
+			'Einen Distelsüchtigen fangen|',
+			'Unterwürfigkeit in Schattenmond|',
+			'Was Illidan will, soll Illidan bekommen...|',
+			'Die Litanei der Verdammnis - Boraks Angriff|',
+			'Die Litanei der Verdammnis - Dritter Teil|',
+			'Die Litanei der Verdammnis|',
+			'Die Prüfung der Naaru: Erbarmen|',
+			'Die Prüfung der Naaru: Stärke|',
+			'Die Prüfung der Naaru: Zuverlässigkeit|',
+			'Die Prüfung der Naaru: Magtheridon|'
+		),
+	'HA' => array( 'Reputation', 'HA' =>
+			'Schlüssel der Auchenai|55216',
+ 			'Unteres Viertel|Respektvoll'
+		 ),
+	'HHC' => array( 'Reputation', 'HHC' => 
+			'Flammengeschmiedeter Schlüssel|55262',
+			'Ehrenfeste|Respektvoll'
+		), 
+	'HCR' => array( 'Reputation', 'HCR' =>
+			'Schlüssel des Kessels|55139',
+			'Expedition des Cenarius|Respektvoll'
+		),
+	'HTK' => array( 'Reputation', 'HTK' =>
+			'Warpgeschmiedeter Schlüssel|55231',
+			'Die Sha\\\'tar|Respektvoll'
+		),
+	'HCoT' => array( 'Reputation', 'HCoT' =>
+			'Schlüssel der Zeit|54481',
+			'Hüter der Zeit|Respektvoll'
+		 ),
+);
+
+
+// HORDE KEYS
+$inst_keys_bc['deDE']['H'] = array(
+	'SL' => array( 'Key-Only', 'SL' =>
+			'Schlüssel des Schattenlabyrinths|57267'
+		),
+	'SH' => array( 'Quests', 'SH' =>
+			'Schlüssel der zerschmetterten Hallen|56320',
+			'Zugang zur Zitadelle|',
+			'Großmeister Rohok|',
+			'Rohoks Bitte|',
+			'Heißer als die Hölle|'
+		),
+	'Arcatraz' => array( 'Quests', 'Arcatraz' =>
+			'Schlüssel zu Arkatraz|58724',
+			'Kristallsammlung des Konsortiums|',
+			'Ein Hügel voll Astraler|',
+			'Sphärenräuber Nesaad|',
+			'Bitte um Unterstätzung|',
+			'Rechtmäßiger Besitz|',
+			'Eine Audienz beim Prinzen|',
+			'Triangulationspunkt Eins|',
+			'Triangulationspunkt Zwei|',
+			'Das volle Dreieck|',
+			'Sonderlieferung nach Shattrath|',
+			'Wie man in Arkatraz einbricht|'
+		),
+	'Kara' => array( 'Quests', 'Kara' =>
+			'Der Schlüssel des Meisters|58887',
+			'Arkane Störungen|',
+			'Rastlose Aktivität|',
+			'Dalarankontaktpersonen||MS',
+			'Khadgar|',
+			'Nach Karazhan|',
+			'Das zweite und das dritte Fragment|',
+			'Die Berührung des Meisters|',
+			'Rückkehr zu Khadgar|'
+		),
+	'TK' => array( 'Quests', 'TK' => 
+			'Schlüssel der Stürme|60250',
+			'Oronok Herzeleid|',
+			'Ich war schon vieles...|',
+			'Eine Lektion gelernt|',
+			'Die Litanei der Verdammnis - Wahrheit und Geschichte|',
+			'Grom\\\'tor, Sohn des Oronok|',
+			'Die Litanei der Verdammnis - Grom\\\'tors Angriff|',
+			'Die Litanei der Verdammnis - Erster Teil|',
+			'Ar\\\'tor, Sohn des Oronok|',
+			'Dämonische Kristallgefängnisse|',
+			'Lohn\\\'goron, Bogen des Herzeleid|',
+			'Die Litanei der Verdammnis - Ar\\\'tors Angriff|',
+			'Die Litanei der Verdammnis - Zweiter Teil|',
+			'Borak, Sohn des Oronok|',
+			'Von Distelköpfen und Eiern...|',
+			'Ein Bündel von Blutdisteln|',
+			'Einen Distelsüchtigen fangen|',
+			'Unterwürfigkeit in Schattenmond|',
+			'Was Illidan will, soll Illidan bekommen...|',
+			'Die Litanei der Verdammnis - Boraks Angriff|',
+			'Die Litanei der Verdammnis - Dritter Teil|',
+			'Die Litanei der Verdammnis|',
+			'Die Prüfung der Naaru: Erbarmen|',
+			'Die Prüfung der Naaru: Stärke|',
+			'Die Prüfung der Naaru: Zuverlässigkeit|',
+			'Die Prüfung der Naaru: Magtheridon|'
+		),
+	'HA' => array( 'Reputation', 'HA' =>
+			'Schlüssel der Auchenai|55216',
+ 			'Unteres Viertel|Respektvoll'
+		 ),
+	'HHC' => array( 'Reputation', 'HHC' => 
+			'Flammengeschmiedeter Schlüssel|55262',
+			'Thrallmar|Respektvoll'
+		), 
+	'HCR' => array( 'Reputation', 'HCR' =>
+			'Schlüssel des Kessels|55139',
+			'Expedition des Cenarius|Respektvoll'
+		),
+	'HTK' => array( 'Reputation', 'HTK' =>
+			'Warpgeschmiedeter Schlüssel|55231',
+			'Die Sha\\\'tar|Respektvoll'
+		),
+	'HCoT' => array( 'Reputation', 'HCoT' =>
+			'Schlüssel der Zeit|54481',
+			'Hüter der Zeit|Respektvoll'
+		 ),
+);
+
+?>
