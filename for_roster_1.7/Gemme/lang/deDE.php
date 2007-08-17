ef803cc9b9062a42832eb5f242303c5e239c9e42
titan99 <titan99@b904fcd1-60ed-da51-8dba-9922f2c0e645>
2007-08-17 10:02:15 +0000

    Gemme: Update following rosterv2

---
+++ for_roster_1.7/Gemme/lang/deDE.php
@@ -0,0 +1,39 @@
+<?php
+// ALLIANCE KEYS
+$wordings['deDE']['gemme_title_addon'] = 'Edelsteine';
+
+$Gem_info['deDE']['Gem_title']="Edelsteine f&uuml;r Sockel";
+$Gem_info['deDE']['Objet']='Objekt';
+$Gem_info['deDE']['Name']='Name';
+$Gem_info['deDE']['level']='Stufe';
+$Gem_info['deDE']['compo']='Reagenzien';
+$Gem_info['deDE']['crafter']='Wer kann es herstellen?';
+$Gem_info['deDE']['Filter']='Filter :';
+$Gem_info['deDE']['NoFilter']='No Filter';
+
+//qualities table rare, uncommon, epic, common, trash
+//$Gem_info['deDE']['qualities']['9d9d9d'] = 'trash';
+//$Gem_info['deDE']['qualities']['ffffff'] = 'common';
+$Gem_info['deDE']['qualities']['1eff00'] = 'uncommon';
+$Gem_info['deDE']['qualities']['0070dd'] = 'rare';
+//$Gem_info['deDE']['qualities']['a335ee'] = 'epic';
+//$Gem_info['deDE']['qualities']['ff8000'] = 'legend';
+
+//Craft Name
+$Gem_info['deDE']['Gem']="Edelsteine";
+$Gem_info['deDE']['sill']="Juwelenschleifen";
+
+//slot name
+$Gem_info['deDE']['slot']="Sockel";
+
+//color
+$Gem_info['deDE']['type']['blue']="blau";
+$Gem_info['deDE']['type']['red']="rot";
+$Gem_info['deDE']['type']['yellow']="gelb";
+$Gem_info['deDE']['type']['meta']="Meta";
+$Gem_info['deDE']['type']['green']="Green";
+$Gem_info['deDE']['type']['orange']="Orange";
+$Gem_info['deDE']['type']['purple']="Purple";
+
+
+?>
