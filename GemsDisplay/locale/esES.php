0b9dc82a6ae06979aa9fe843b9a1fba42e530808
titan99 <titan99@b904fcd1-60ed-da51-8dba-9922f2c0e645>
2007-08-17 11:25:06 +0000

    GemsDisplay for roster V2

---
+++ GemsDisplay/locale/esES.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * WoWRoster.net WoWRoster
+ *
+ * LICENSE: Licensed under the Creative Commons
+ *          "Attribution-NonCommercial-ShareAlike 2.5" license
+ *
+ * @copyright  2002-2007 WoWRoster.net
+ * @license    http://creativecommons.org/licenses/by-nc-sa/2.5   Creative Commons "Attribution-NonCommercial-ShareAlike 2.5"
+ * @version    SVN: $ v0.5 Titan99 and Tyradil $
+ * @link       http://www.wowroster.net
+ * @package    GemsDisplay
+ * @subpackage Installer
+*/
+$lang['GemsDisplay_menu'] = 'Gemas|Display Gems craft';
+$lang['gemme_title_addon'] = 'Gemas';
+
+$lang['Gem_title']="Gemas para la Ranura";
+$lang['Objet']='Objeto';
+$lang['Name']='Nombre';
+$lang['level']='Nivel';
+$lang['compo']='Reagentes';
+$lang['crafter']='Quien puede hacermelo';
+$lang['Filter']='Filtre :';
+$lang['NoFilter']='Non Filter';
+
+//qualities table rare, uncommon, epic, common, trash
+//$lang['qualities']['9d9d9d'] = 'trash';
+//$lang['qualities']['ffffff'] = 'common';
+$lang['qualities']['1eff00'] = 'uncommon';
+$lang['qualities']['0070dd'] = 'rare';
+//$lang['qualities']['a335ee'] = 'epic';
+//$lang['qualities']['ff8000'] = 'legend';
+
+//Craft Name
+$lang['Gem']="Gemas";
+$lang['sill']="Joyeria";
+
+//slot name
+$lang['slot']="Ranura";
+
+//color
+$lang['type']['blue']="Azul";
+$lang['type']['red']="Rojo";
+$lang['type']['yellow']="Amarillo";
+$lang['type']['meta']="meta";
+$lang['type']['green']="Green";
+$lang['type']['orange']="Orange";
+$lang['type']['purple']="Purple";
+
+
+
+?>
