d2a395f3f1a8de4b49c8659eb8527c4d459c5cbe
titan99 <titan99@b904fcd1-60ed-da51-8dba-9922f2c0e645>
2007-08-17 11:25:06 +0000

    GemsDisplay for roster V2

---
+++ trunk/GemsDisplay/locale/deDE.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * WoWRoster.net WoWRoster
+ *
+ * LICENSE: Licensed under the Creative Commons
+ *          "Attribution-NonCommercial-ShareAlike 2.5" license
+ *
+ * @copyright  2002-2007 WoWRoster.net
+ * @license    http://creativecommons.org/licenses/by-nc-sa/2.5   Creative Commons "Attribution-NonCommercial-ShareAlike 2.5"
+ * @version    SVN: $ v0.5 Titan99 and Tyradil $
+ * @link       http://www.wowroster.net
+ * @package    GemsDisplay
+ * @subpackage Installer
+*/
+$lang['GemsDisplay_menu'] = 'Edelsteine|Affiche les gemmes crafter dans la guilde';
+$lang['gemme_title_addon'] = 'Edelsteine';
+
+$lang['Gem_title']="Edelsteine f&uuml;r Sockel";
+$lang['Objet']='Objekt';
+$lang['Name']='Name';
+$lang['level']='Stufe';
+$lang['compo']='Reagenzien';
+$lang['crafter']='Wer kann es herstellen?';
+$lang['Filter']='Filter :';
+$lang['NoFilter']='No Filter';
+
+//qualities table rare, uncommon, epic, common, trash
+//$lang['qualities']['9d9d9d'] = 'trash';
+//$lang['qualities']['ffffff'] = 'common';
+$lang['qualities']['1eff00'] = 'uncommon';
+$lang['qualities']['0070dd'] = 'rare';
+//$lang['qualities']['a335ee'] = 'epic';
+//$lang['qualities']['ff8000'] = 'legend';
+
+//Craft Name
+$lang['Gem']="Edelsteine";
+$lang['sill']="Juwelenschleifen";
+
+//slot name
+$lang['slot']="Sockel";
+
+//color
+$lang['type']['blue']="blau";
+$lang['type']['red']="rot";
+$lang['type']['yellow']="gelb";
+$lang['type']['meta']="Meta";
+$lang['type']['green']="Green";
+$lang['type']['orange']="Orange";
+$lang['type']['purple']="Purple";
+
+
+?>
