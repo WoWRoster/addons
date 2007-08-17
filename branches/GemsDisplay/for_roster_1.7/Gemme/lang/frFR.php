b10c7f794c2d96116dfad4b3ca03de1e684e42ad
titan99 <titan99@b904fcd1-60ed-da51-8dba-9922f2c0e645>
2007-08-17 10:02:15 +0000

    Gemme: Update following rosterv2

---
+++ branches/GemsDisplay/for_roster_1.7/Gemme/lang/frFR.php
@@ -0,0 +1,38 @@
+<?php
+// ALLIANCE KEYS
+$wordings['frFR']['gemme_title_addon'] = 'Gemmes';
+
+$Gem_info['frFR']['Gem_title']="Gemmes pour Châsse";
+$Gem_info['frFR']['Objet']='Objet';
+$Gem_info['frFR']['Name']='Nom';
+$Gem_info['frFR']['level']='Niveau';
+$Gem_info['frFR']['compo']='Réactifs';
+$Gem_info['frFR']['crafter']='Artisants';
+$Gem_info['frFR']['Filter']='Filtre sur la qualité:';
+$Gem_info['frFR']['NoFilter']='Non Filtré';
+
+//qualities table rare, uncommon, epic, common, trash
+//$Gem_info['frFR']['qualities']['9d9d9d'] = 'Médiocre';
+//$Gem_info['frFR']['qualities']['ffffff'] = 'Classique';
+$Gem_info['frFR']['qualities']['1eff00'] = 'Bonne qualité';
+$Gem_info['frFR']['qualities']['0070dd'] = 'Supérieure';
+//$Gem_info['frFR']['qualities']['a335ee'] = 'Epique';
+//$Gem_info['frFR']['qualities']['ff8000'] = 'Légendaire';
+
+//Craft Name
+$Gem_info['frFR']['Gem']="Gemmes";
+$Gem_info['frFR']['sill']="Joaillerie";
+
+//slot name
+$Gem_info['frFR']['slot']="châsse";
+
+//color
+$Gem_info['frFR']['type']['blue']="Bleue";
+$Gem_info['frFR']['type']['red']="Rouge";
+$Gem_info['frFR']['type']['yellow']="Jaune";
+$Gem_info['frFR']['type']['green']="Verte";
+$Gem_info['frFR']['type']['orange']="Orange";
+$Gem_info['frFR']['type']['purple']="Violette";
+$Gem_info['frFR']['type']['meta']="Méta";
+
+?>
