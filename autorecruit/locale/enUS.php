cd7f3fc6df1b541860403b2a8680f93e2a0b6c5b
Adric <Adric@b904fcd1-60ed-da51-8dba-9922f2c0e645>
2007-10-07 23:17:05 +0000

    AutoRecruit - Bug fix for no guilds showing for config...

--- autorecruit/locale/enUS.php
+++ autorecruit/locale/enUS.php
@@ -19,8 +19,6 @@ $lang['autorecruit'] = 'AutoRecruit';
 $lang['autorecruit_desc'] = 'Automaticaly sets recruitment status per class using the roster';
 $lang['arecruitbutton'] = 'Recruitment|Shows what classes the guild is recruiting';
 
-
-
 // Config strings
 $lang['admin']['autorecruit_conf']			= 'AutoRecruit Configuration|Configure base settings for AutoRecruit';
 $lang['admin']['autorecruit_minlevel']		= 'Minimum Level|Minimum level of characters to count towards class totals';
@@ -34,24 +32,13 @@ $lang['admin']['threshold'] = 'Threshold';
 $lang['admin']['min_level'] = 'Minimum Level';
 $lang['admin']['app_link'] = 'Application URL';
 
-$lang['admin']['autorecruit_druid']		= 'Max Druids|Maximum number of Druids';
-$lang['admin']['autorecruit_hunter']		= 'Max Hunters|Maximum number of Hunters';
-$lang['admin']['autorecruit_mage']		= 'Max Mages|Maximum number of Mages';
-$lang['admin']['autorecruit_paladin']		= 'Max Paladins|Maximum number of Paladins';
-$lang['admin']['autorecruit_priest']		= 'Max Priests|Maximum number of Priests';
-$lang['admin']['autorecruit_rogue']		= 'Max Rogues|Maximum number of Rogues';
-$lang['admin']['autorecruit_shaman']		= 'Max Shamans|Maximum number of Shamans';
-$lang['admin']['autorecruit_warlock']		= 'Max Warlocks|Maximum number of Warlocks';
-$lang['admin']['autorecruit_warrior']		= 'Max Warriors|Maximum number of Warriors';
-
-
 // Guild Page Set
 $lang['ar_page']['title']		= 'Class Recruitment';
 $lang['ar_page']['class']		= 'Class';
 $lang['ar_page']['status']		= 'Status';
 $lang['ar_page']['apply']		= 'Apply';
 
-// Errors
+// Setup
 $lang['ar_page']['no_guild']		= 'No guild selected.';
 $lang['ar_page']['no_setup']		= 'AutoRecruit has not been setup for this guild.';
 
