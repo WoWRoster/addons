6ebe44e0868b7d5a9510033fc2adc693299082b7
poetter <poetter@b904fcd1-60ed-da51-8dba-9922f2c0e645>
2007-10-09 00:51:58 +0000

    ArmorySync: svn[264] - once again a rewrite of doUpdateStatus in armorysync.js - once again restructured XML Data - implemented debugging infos for all methods - implemented sql debugging - restructured config menu - added java effects for images - made images configurable from menu

--- trunk/armorysync/inc/armorysynctalents.class.php
+++ trunk/armorysync/inc/armorysynctalents.class.php
@@ -19,20 +19,26 @@ if( !defined('IN_ROSTER') )
     exit('Detected invalid access to this file!');
 }
 
-class ArmorySyncTalents {
-    
+require_once ($addon['dir'] . 'inc/armorysyncbase.class.php');
+
+class ArmorySyncTalents extends ArmorySyncBase {
+
     var $host;
     var $url;
-    
+
+    var $debugmessages = array();
+    var $errormessages = array();
+
     function getTalents( $class ) {
         global $roster;
-        
+
         $locale = substr($roster->config['locale'], 0, 2);
         $cache_tag = $class.$locale;
-        
-        if( $roster->cache->check($cache_tag) )
+
+        $ret = '';
+		if( $roster->cache->check($cache_tag) )
         {
-            return $roster->cache->get($cache_tag);
+            $ret = $roster->cache->get($cache_tag);
         }
         else
         {
@@ -41,24 +47,28 @@ class ArmorySyncTalents {
             $content = $this->_prepareData( $content );
             $data = $this->_getData( $content );
             $roster->cache->put($data, $cache_tag);
-            return $data;
+            $ret = $data;
         }
+		$this->_debug( $ret ? 2 : 0, $ret, 'Fetch talent tree icons from blizzard', $ret ? 'OK' : 'Failed' );
+		return $ret;
     }
-    
+
     function _getData( $content = false ) {
-        
-        eval( $content );
-        
-        return array(
+
+        $check = eval( $content );
+
+        $ret = array(
                         'tree' => $tree,
                         'talent' => $talent,
                         'rank' => $rank,
                         'treeStartStop' => $treeStartStop,
                     );
+		$this->_debug( is_null($check)  ? 3 : 0, array($check, $ret), 'Eval converted java code', is_null($check) ? 'OK' : 'Failed' );
+		return $ret;
     }
-    
+
     function _prepareData( $content = false ) {
-        
+
         $content = str_replace('i =', '$i =', $content );
         $content = str_replace('i++ ', 'i++;', $content );
         $content = str_replace('i++', '$i++', $content );
@@ -71,69 +81,69 @@ class ArmorySyncTalents {
 
         $content = str_replace('className', '$className', $content );
         $content = str_replace('talentPath', '$talentPath', $content );
-        
+
         $content = str_replace('tree[', '$tree[', $content );
         $content = str_replace('talent[', '$talent[', $content );
         $content = str_replace('rank[', '$rank[', $content );
-        
+
         $content = preg_replace('/, \[.*?\]/', '', $content, -1);
-        
+
         $content = str_replace('var ', '', $content );
         $content = str_replace('treeStartStop', '$treeStartStop', $content );
         $content = str_replace('jsLoaded', '//jsLoaded', $content );
-        
+
         $content = str_replace('= [', '= array(', $content );
         $content = str_replace('=[', '= array(', $content );
         $content = str_replace('];', ');', $content );
-        
+
+		$this->_debug( $content ? 3 : 0, $content, 'Convert java code to php', $content ? 'OK' : 'Failed' );
         return $content;
     }
-    
+
     function _fetchData() {
         global $roster;
 	$useragent = "User-Agent: Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.2) Gecko/20070219 Firefox/2.0.0.2";
         $cookie = "cookieLangId=".strtolower(substr($roster->config['locale'], 0, 2)) . "_" . strtolower(substr($roster->config['locale'], 2, 2)) .";";
         $content = '';
 
-	
-	
-	if ($fp = fsockopen($this->host, 80, $errno, $errstr, 30)) {
-		stream_set_timeout($fp, 30);
-		$buffer = '';
-		
-		$headers = 
-			"GET ". $this->url. " HTTP/1.0\r\n" .
-			"Host: ". $this->host. "\r\n".
-			"Connection: close\r\n".
-			"Cookie: $cookie\r\n".
-			"\r\n";
-		
-		fwrite($fp, $headers);
-		while (!feof($fp)) {
-			$content .= fread($fp, 8192);       
-		}
-		fclose($fp);
-	} else {
-		//Throw Error
-		print "CRITICAL ERROR - socket not connected";
-	}
+		if ($fp = fsockopen($this->host, 80, $errno, $errstr, 30)) {
+			stream_set_timeout($fp, 30);
+			$buffer = '';
 
-	//If not using CURL we may get the header returned before the HTML table..
-	$pos = $this->_stripos_b ($content, 'var i =');
-	if ($pos != 0){
-		//Need to trim header off..
-		$content = substr ($content, $pos);
-	}
-        return $content;
+			$headers =
+				"GET ". $this->url. " HTTP/1.0\r\n" .
+				"Host: ". $this->host. "\r\n".
+				"Connection: close\r\n".
+				"Cookie: $cookie\r\n".
+				"\r\n";
+
+			fwrite($fp, $headers);
+			while (!feof($fp)) {
+				$content .= fread($fp, 8192);
+			}
+			fclose($fp);
+		} else {
+			//Throw Error
+			print "CRITICAL ERROR - socket not connected";
+		}
+
+		//If not using CURL we may get the header returned before the HTML table..
+		$pos = $this->_stripos_b ($content, 'var i =');
+		if ($pos != 0){
+			//Need to trim header off..
+			$content = substr ($content, $pos);
+		}
+		$this->_debug( $content ? 3 : 0, $content, 'Fetch java code from blizzard', $content ? 'OK' : 'Failed' );
+		return $content;
     }
-    
-    
+
+
     function _makeURL ( $class = false ) {
         global $roster;
-        
+
         $host = '';
         $url = '';
-        
+
         $locale = substr($roster->config['locale'], 0, 2);
 
         if( $locale == 'en' )
@@ -146,9 +156,12 @@ class ArmorySyncTalents {
                 $this->host = 'www.wow-europe.com';
                 $this->url = '/shared/global/talents/'. $class. '/'. $locale. '/data.js';
         }
+		$this->_debug( 3, null, 'Choose server and url', 'OK');
     }
 
     function _stripos_b($haystack, $needle){
-        return strpos($haystack, stristr( $haystack, $needle ));
+        $ret = strpos($haystack, stristr( $haystack, $needle ));
+		$this->_debug( $ret ? 3 : 0, $ret, 'Find needle in haystack', $ret ? 'OK' : 'Failed' );
+		return $ret;
     }
 }
