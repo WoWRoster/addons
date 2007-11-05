32adeeb4028adbbac46aa841bcc7f50ef3fc2b27
mdeshane <mdeshane@b904fcd1-60ed-da51-8dba-9922f2c0e645>
2007-11-05 17:38:08 +0000

    Accounts:  - First commit: can register, login, and use some access control. Primarily a developer preview of the addon.

---
+++ trunk/accounts/locale/deDE.php
@@ -0,0 +1,53 @@
+<?php
+/** 
+ * Dev.PKComp.net WoWRoster Addon
+ * 
+ * LICENSE: Licensed under the Creative Commons 
+ *          "Attribution-NonCommercial-ShareAlike 2.5" license 
+ * 
+ * @copyright  2005-2007 Pretty Kitty Development 
+ * @license    http://creativecommons.org/licenses/by-nc-sa/2.5   Creative Commons "Attribution-NonCommercial-ShareAlike 2.5" 
+ * @link       http://dev.pkcomp.net 
+ * @package    Accounts 
+ * @subpackage Locale deDE
+ */ 
+
+// -[ deDE Localization ]- 
+
+// User class messages
+$lang['account_user']['msg10'] = "Login und/oder Passwort finden keinen Treffer in der Datenbank.";
+$lang['account_user']['msg11'] = "Login und/oder Passwort sind leer!";
+$lang['account_user']['msg12'] = "Leider existiert bereits ein Benutzer mit diesem Login und/oder E-mailadresse.";
+$lang['account_user']['msg13'] = "Weitere Anweisungen wurden per E-mail versandt, folgen Sie nun den Instruktionen.";
+$lang['account_user']['msg14'] = "Es is ein Fehler entstanden probieren Sie es erneut.";
+$lang['account_user']['msg15'] = "Es is ein Fehler entstanden probieren Sie es später nochmal.";
+$lang['account_user']['msg16'] = "Die eingegebene E-mailadresse ist nicht gültig.";
+$lang['account_user']['msg17'] = "Das Feld login (min. %d Zeichen) muss eingegeben sein.";
+$lang['account_user']['msg18'] = "Ihr Benutzerkonto ist aktiv. Sie können sich nun anmelden.";
+$lang['account_user']['msg19'] = "Ihr Aktivierungs ist nicht gültig.";
+$lang['account_user']['msg20'] = "Da ist kein Konto zu aktivieren.";
+$lang['account_user']['msg21'] = "Der benutzte Aktivierung-Code is nicht gültig!";
+$lang['account_user']['msg22'] = "Keine Konto gefunden dass mit der eingegeben E-mailadresse übereinkommt.";
+$lang['account_user']['msg23'] = "Kontrollieren Sie Ihre E-Mail um Ihr neues Passwort zu erhalten.";
+$lang['account_user']['msg25'] = "Kann Ihr Passwort nicht aktivieren.";
+$lang['account_user']['msg26'] = "";
+$lang['account_user']['msg27'] = "Kontrollieren Sie Ihre E-Mailbox und bestätigen Sie Ihre Änderung(en).";
+$lang['account_user']['msg28'] = "Ihre Anfrage bestätigen...";
+$lang['account_user']['msg29'] = "Hallo %s,\r\n\r\num Ihre Anfrage zu aktivieren klicken Sie bitte auf den folgenden Link:\r\n%s&uid=%d&act_key=%s";
+$lang['account_user']['msg30'] = "Ihre Änderung ist durchgeführt.";
+$lang['account_user']['msg31'] = "Diese E-mailadresse wird bereits genutzt, bitte wählen Sie eine andere.";
+$lang['account_user']['msg32'] = "Das Feld Passwort (min. %d Zeichen) muss eingegeben sein.";
+$lang['account_user']['msg33'] = "Hallo %s,\r\n\r\nIhre neue E-mailadresse muss noch überprüft werden, bitte klicken Sie auf den folgenden Link:\r\n%s&uid=%d&validate=%s";
+$lang['account_user']['msg34'] = "Da ist keine E-mailadresse zu überprüfen.";
+$lang['account_user']['msg35'] = "Hallo %s,\r\n\r\nIhr neues Passwort kann nun eingegeben werden, bitte klicken Sie auf den folgenden Link:\r\n%s&uid=%d&act_key=%s";
+$lang['account_user']['msg36'] = "Ihr Antrag ist verarbeitet und wird nun durch den Administrator kontrolliert. \r\nSie erhalten eine Nachricht wenn dies geschehen ist.";
+$lang['account_user']['msg37'] = "Hallo %s,\r\n\r\nIhr Konto ist nun eigerichtet und Sie können sich anmelden.\r\n\r\nKlicken Sie hierfür auf den folgenden Link:\r\n%s\r\n\r\nmit freundlichen Grüssen\r\n%s";
+$lang['account_user']['msg38'] = "Das best&auml;tigte Passwort hat keine &Uuml;bereinstimmung mit dem ersten Passwort, bitte probieren Sie es erneut.";
+
+
+// Profile class messages
+$lang['account_profile']['msg1'] = "Zur Zeit verf&uuml;gt dieses Konto über keine weiteren Profildaten.";
+$lang['account_profile']['msg2'] = "Die Profildaten wurden ge&auml;ndert.";
+$lang['account_profile']['msg3'] = "Es ist ein Fehler entstanden, bitte probieren Sie es erneut.";
+
+?>
