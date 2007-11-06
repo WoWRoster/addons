7d15f95916ddeeac074e1f3bb29d718a60f686f5
Zanix <Zanix@b904fcd1-60ed-da51-8dba-9922f2c0e645>
2007-11-06 20:04:58 +0000

    Accounts: - Code formatting - Changed hard coded links to use makelink() - Changed -- include_once ($addon['dir'] . 'inc' . DIR_SEP . 'user.lib.php'); -- include_once ($addon['inc_dir'] . 'user.lib.php'); - Changed -- $roster->output['show_menu'] = array('main','util','guild','realm','account_menu'); -- $roster->output['show_menu'][] = 'account_menu'; - Removed ?> off the end of all files - Some small xhtml fixes - changed installer to use $installer->create_table() and $installer->drop_table() - Fixed text box sizes in admin

--- trunk/accounts/inc/profile.lib.php
+++ trunk/accounts/inc/profile.lib.php
@@ -1,86 +1,110 @@
-<?php 
-/** 
+<?php
+/**
  * Dev.PKComp.net WoWRoster Addon
- * 
- * LICENSE: Licensed under the Creative Commons 
- *          "Attribution-NonCommercial-ShareAlike 2.5" license 
- * 
- * @copyright  2005-2007 Pretty Kitty Development 
- * @license    http://creativecommons.org/licenses/by-nc-sa/2.5   Creative Commons "Attribution-NonCommercial-ShareAlike 2.5" 
- * @link       http://dev.pkcomp.net 
- * @package    Accounts 
- * @subpackage Profile 
+ *
+ * LICENSE: Licensed under the Creative Commons
+ *          "Attribution-NonCommercial-ShareAlike 2.5" license
+ *
+ * @copyright  2005-2007 Pretty Kitty Development
+ * @license    http://creativecommons.org/licenses/by-nc-sa/2.5   Creative Commons "Attribution-NonCommercial-ShareAlike 2.5"
+ * @link       http://dev.pkcomp.net
+ * @package    Accounts
+ * @subpackage Profile
  */
 if( !defined('IN_ROSTER') )
 {
-    exit('Detected invalid access to this file!');
+	exit('Detected invalid access to this file!');
 }
 
-include ('user.lib.php'); 
+include_once ($addon['inc_dir'] . 'user.lib.php');
 
-class userProfile extends accountUser {
-	
-	var $profileTable = utable; 
-	
+class userProfile extends accountUser
+{
+	var $profileTable = utable;
+
 	var $uid;
 	var $city;
 	var $country;
 	var $homepage;
 	var $notes;
-	
-	function createFormField($formelement, $label, $length = 25, $required = false) {
-		$form_field = "<label for=\"".$formelement."\">".$label."</label>\n";
-		$form_field .= "  <input name=\"".$formelement."\" type=\"text\" size=\"".$length."\" value=\"";
-		if (isset($_REQUEST[$formelement])) {
+
+	function createFormField($formelement, $label, $length = 25, $required = false)
+	{
+		$form_field = '<label for="' . $formelement . '">' . $label . "</label>\n";
+		$form_field .= '  <input name="' . $formelement . '" type="text" size="' . $length . '" value="';
+		if (isset($_REQUEST[$formelement]))
+		{
 			$form_field .= $_REQUEST[$formelement];
-		} elseif (isset($this->$formelement) && !isset($_REQUEST[$formelement])) {
+		}
+		elseif (isset($this->$formelement) && !isset($_REQUEST[$formelement]))
+		{
 			$form_field .= $this->$formelement;
-		} else {
-			$form_field .= "";
 		}
-		$form_field .= ($required) ? "\">*<br>\n" : "\"><br>\n";
-		return $form_field;		
+		else
+		{
+			$form_field .= '';
+		}
+		$form_field .= ($required) ? "\">*<br />\n" : "\"><br />\n";
+		return $form_field;
 	}
-	
-	function createTextArea($text_field, $label) {
-		$textarea = "<label for=\"".$text_field."\">".$label."</label>\n";
-		$textarea .= "  <textarea name=\"".$text_field."\">";
-		if (isset($_REQUEST[$text_field])) {
+
+	function createTextArea($text_field, $label)
+	{
+		$textarea = '<label for="' . $text_field . '">' . $label . "</label>\n";
+		$textarea .= '  <textarea name="' . $text_field . '">';
+		if (isset($_REQUEST[$text_field]))
+		{
 			$textarea .= $_REQUEST[$text_field];
-		} elseif (isset($this->$text_field)) {
-			$textarea .= $this->$text_field;
-		} else {
-			$textarea .= "";
 		}
-		$textarea .= "</textarea><br>\n";
-		return $textarea;		
+		elseif (isset($this->$text_field))
+		{
+			$textarea .= $this->$text_field;
+		}
+		else
+		{
+			$textarea .= '';
+		}
+		$textarea .= "</textarea><br />\n";
+		return $textarea;
 	}
-	
-	function saveProfileDate($ident = "", $lname = "", $city = "", $country = "", $hp = "", $notes = "") {
-		if ($_SESSION['is_rec']) {
-			$sql = sprintf("UPDATE %s SET `lname` = %s `city` = %s, `country` = %s, `homepage` = %s, `notes` = %s, `last_change` = NOW() WHERE `uid` = %d AND `uname` = %s", $this->profileTable, $this->ins_string($lname), $this->ins_string($city), $this->ins_string($country), $this->ins_string($hp), $this->ins_string($notes), $this->ins_string($ident, "int"), $_SESSION['user']);
-		} else {
-			$sql = sprintf("INSERT INTO %s (`uid`, `uname`, `lname`, `city`, `country`, `homepage`, `notes`, `last_change`) VALUES (NULL, %s, %s, %s, %s, %s, NOW())", $this->profileTable, $this->ins_string($ident, "int"), $_SESSION['user'], $this->ins_string($lname), $this->ins_string($city), $this->ins_string($country), $this->ins_string($hp), $this->ins_string($notes));
-		}	
-		if ($roster->db->query($sql) or die ($roster->db->errno() ':' $roster->db->error())) {
+
+	function saveProfileDate($ident = '', $lname = '', $city = '', $country = '', $hp = '', $notes = '')
+	{
+		if ($_SESSION['is_rec'])
+		{
+			$sql = sprintf("UPDATE %s SET `lname` = %s `city` = %s, `country` = %s, `homepage` = %s, `notes` = %s, `last_change` = NOW() WHERE `uid` = %d AND `uname` = %s", $this->profileTable, $this->ins_string($lname), $this->ins_string($city), $this->ins_string($country), $this->ins_string($hp), $this->ins_string($notes), $this->ins_string($ident, 'int'), $_SESSION['user']);
+		}
+		else
+		{
+			$sql = sprintf("INSERT INTO %s (`uid`, `uname`, `lname`, `city`, `country`, `homepage`, `notes`, `last_change`) VALUES (NULL, %s, %s, %s, %s, %s, NOW())", $this->profileTable, $this->ins_string($ident, 'int'), $_SESSION['user'], $this->ins_string($lname), $this->ins_string($city), $this->ins_string($country), $this->ins_string($hp), $this->ins_string($notes));
+		}
+		if ($roster->db->query($sql) or die ($roster->db->errno() . ':' . $roster->db->error()))
+		{
 			$this->uid = (!$_SESSION['is_rec']) ? $roster->db->insert_id() : $ident;
 			$this->message = $roster->locale->act['account_profile']['msg2'];
-		} else {
+		}
+		else
+		{
 			$this->message = $roster->locale->act['account_profile']['msg3'];
 		}
 	}
-	
-	function getProfileData() {
+
+	function getProfileData()
+	{
 		$this->getUserInfo();
 		$_SESSION['uid'] = $this->uid;
 		$sql = sprintf("SELECT `uid`, `lname`, `city`, `country`, `homepage`, `notes` FROM %s WHERE `uname` = %s", $this->profileTable, $this->user);
-		$result = $roster->db->query($sql) or die ($roster->db->errno() ':' $roster->db->error());
-		if ($roster->db->num_rows($result) == 0) {
-			$_SESSION['is_rec'] = false;			
-			$this->message = $roster->locale->act['account_profile']['msg1'];
-		} else {
+		$result = $roster->db->query($sql) or die ($roster->db->errno() . ':' . $roster->db->error());
+		if ($roster->db->num_rows($result) == 0)
+		{
+			$_SESSION['is_rec'] = false;
+		$this->message = $roster->locale->act['account_profile']['msg1'];
+		}
+		else
+		{
 			$_SESSION['is_rec'] = true; // this session acts like an extra control
-			while ($obj = $roster->db->fetch($result)) {
+			while ($obj = $roster->db->fetch($result))
+			{
 				$this->uid = $obj->uid;
 				$this->userLName = $obj->lname;
 				$this->city = $obj->city;
@@ -90,6 +114,4 @@ class userProfile extends accountUser {
 			}
 		}
 	}
-
 }
-?>
--- trunk/accounts/inc/update_hook.php
+++ trunk/accounts/inc/update_hook.php
@@ -93,5 +93,3 @@ class accountsUpdate
 		return true;
 	}
 }
-
-?>
