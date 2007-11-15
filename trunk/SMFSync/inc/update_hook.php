eddf384daafc946625451bc6ff4a1ae21d314b37
boyo <boyo@b904fcd1-60ed-da51-8dba-9922f2c0e645>
2007-11-15 03:50:29 +0000

    [SMFSync] -Thank you Zanix! --Fixed having to use the original author's LUA parser.

--- trunk/SMFSync/inc/update_hook.php
+++ trunk/SMFSync/inc/update_hook.php
@@ -54,7 +54,7 @@ class smfsyncUpdate
 
 	function update()
 	{
-		global $roster;
+		global $roster, $update;
 
 
 		$CALENDAR_TABLE = "group_calendar_info";
@@ -83,12 +83,8 @@ class smfsyncUpdate
 					unlink($filename);
 
 					//Parse the LUA file into an array
-					//$luahandler = new lua();
-
-					//$lua_data = $luahandler->luatophp($file_as_array);
-					//I attempted to use the built in lua parser, but it just didnt work for some reason
-					//and to keep me from getting a headache I just gave up and use the original authors work.
-					$lua_data = $this->ParseLuaArray($file_as_array);
+					//THANK YOU ZANIX!!!!!
+					$lua_data = $update->uploadData['groupcalendar'];
 
 					unset($file_as_array);
 
@@ -819,108 +815,4 @@ class smfsyncUpdate
 	  return $calendar_items;
 	}
 
-	function ParseLuaArray( &$file_as_array )
-	{
-		if( !is_array($file_as_array) )
-		{
-			// return false if not presented with an array
-			return(false);
-		}
-		else
-		{
-			// Parse the contents of the array
-			$stack = array( array( '',  array() ) );
-			$stack_pos = 0;
-			$last_line = '';
-			foreach( $file_as_array as $line )
-			{
-				// join lines ending in \\ together
-				if( substr( $line, -2, 1 ) == '\\' )
-				{
-					$last_line .= substr($line, 0, -2) . "\n";
-					continue;
-				}
-				if($last_line!='')
-				{
-					$line = trim($last_line . $line);
-					$last_line = '';
-				}
-				else
-				{
-					$line = trim($line);
-				}
-
-				// Look for end of an array
-				if( $line[0] == '}' )
-				{
-					$hash = $stack[$stack_pos];
-					unset($stack[$stack_pos]);
-					$stack_pos--;
-					$stack[$stack_pos][1][$hash[0]] = $hash[1];
-					unset($hash);
-				}
-				// Handle other cases
-				else
-				{
-					// Check if the key is given
-					if( strpos($line,'=') )
-					{
-						list($name, $value) = explode( '=', $line, 2 );
-						$name = trim($name);
-						$value = trim($value,', ');
-						if($name[0]=='[')
-						{
-							$name = trim($name, '[]"');
-						}
-					}
-					// Otherwise we'll have to make one up for ourselves
-					else
-					{
-						$value = $line;
-						if( empty($stack[$stack_pos][1]) )
-						{
-							$name = 1;
-						}
-						else
-						{
-							$name = max(array_keys($stack[$stack_pos][1]))+1;
-						}
-						if( strpos($line,'-- [') )
-						{
-							$value = explode('-- [',$value);
-							array_pop($value);
-							$value = implode('-- [',$value);
-						}
-						$value = trim($value,', ');
-					}
-					if( $value == '{' )
-					{
-						$stack_pos++;
-						$stack[$stack_pos] = array($name, array());
-					}
-					else
-					{
-						if($value[0]=='"')
-						{
-							$value = substr($value,1,-1);
-						}
-						else if($value == 'true')
-						{
-							$value = true;
-						}
-						else if($value == 'false')
-						{
-							$value = false;
-						}
-						else if($value == 'nil')
-						{
-							$value = NULL;
-						}
-						$stack[$stack_pos][1][$name] = $value;
-					}
-				}
-			}
-			return($stack[0][1]);
-		}
-	}
 }
