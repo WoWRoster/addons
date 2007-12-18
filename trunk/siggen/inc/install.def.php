79424a8b95391eabbdf3e8102ffd397c70b4363a
Zanix <Zanix@b904fcd1-60ed-da51-8dba-9922f2c0e645>
2007-12-18 01:11:26 +0000

    siggen: - install.def.php didn't get the new version number :(

--- trunk/siggen/inc/install.def.php
+++ trunk/siggen/inc/install.def.php
@@ -31,7 +31,7 @@ class siggenInstall
 	var $active = true;
 	var $icon = 'inv_misc_book_13';
 
-	var $version = '0.3.0.322';
+	var $version = '0.3.0.328';
 	var $wrnet_id = '20';
 
 	var $fullname = 'SigGen';
