dc73babdeecf00cb12740e7aebf889433f6c13de
Joe Foster <ulminia@gmail.com>
2009-03-06 19:53:30 +0000

    ArmorySync: - update fixed no update issue couldent totaly remove use of armory class but its allmost there...

---
+++ armorysync/inc/talenticons_priest.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * WoWRoster.net WoWRoster
+ *
+ * Talent Icons for priests
+ *
+ * LICENSE: Licensed under the Creative Commons
+ *          "Attribution-NonCommercial-ShareAlike 2.5" license
+ *
+ * @copyright  2002-2007 WoWRoster.net
+ * @license    http://creativecommons.org/licenses/by-nc-sa/2.5   Creative Commons "Attribution-NonCommercial-ShareAlike 2.5"
+ * @version    SVN: $Id$
+ * @link       http://www.wowroster.net
+ * @package    ArmorySync
+*/
+
+if ( !defined('IN_ROSTER') )
+{
+	exit('Detected invalid access to this file!');
+}
+
+$talentIcons["priest"][1][1][2] = "spell_nature_manaregentotem";
+$talentIcons["priest"][1][1][3] = "spell_holy_powerwordshield";
+$talentIcons["priest"][1][1][4] = "spell_holy_absolution";
+$talentIcons["priest"][1][1][6] = "spell_shadow_focusedpower";
+$talentIcons["priest"][1][1][7] = "spell_arcane_focusedpower";
+$talentIcons["priest"][1][1][8] = "spell_holy_holyprotection";
+$talentIcons["priest"][1][1][9] = "spell_holy_devineaegis";
+$talentIcons["priest"][1][2][1] = "spell_magic_magearmor";
+$talentIcons["priest"][1][2][2] = "spell_holy_innerfire";
+$talentIcons["priest"][1][2][3] = "spell_frost_windwalkon";
+$talentIcons["priest"][1][2][4] = "ability_hibernation";
+$talentIcons["priest"][1][2][5] = "spell_nature_enchantarmor";
+$talentIcons["priest"][1][2][7] = "spell_holy_powerinfusion";
+$talentIcons["priest"][1][2][8] = "spell_holy_rapture";
+$talentIcons["priest"][1][2][9] = "spell_holy_painsupression";
+$talentIcons["priest"][1][2][10] = "spell_holy_borrowedtime";
+$talentIcons["priest"][1][2][11] = "spell_holy_penance";
+$talentIcons["priest"][1][3][1] = "spell_holy_sealofvengeance";
+$talentIcons["priest"][1][3][2] = "spell_holy_wordfortitude";
+$talentIcons["priest"][1][3][3] = "spell_nature_sleep";
+$talentIcons["priest"][1][3][5] = "spell_holy_divinespirit";
+$talentIcons["priest"][1][3][6] = "spell_arcane_mindmastery";
+$talentIcons["priest"][1][3][7] = "spell_holy_powerwordshield";
+$talentIcons["priest"][1][3][8] = "spell_holy_aspiration";
+$talentIcons["priest"][1][3][9] = "spell_holy_hopeandgrace";
+$talentIcons["priest"][1][4][2] = "spell_nature_tranquility";
+$talentIcons["priest"][1][4][4] = "spell_shadow_manaburn";
+$talentIcons["priest"][1][4][5] = "spell_holy_divinespirit";
+
+$talentIcons["priest"][2][1][1] = "spell_holy_healingfocus";
+$talentIcons["priest"][2][1][3] = "spell_holy_restoration";
+$talentIcons["priest"][2][1][4] = "spell_holy_purify";
+$talentIcons["priest"][2][1][5] = "spell_holy_prayerofhealing02";
+$talentIcons["priest"][2][1][6] = "spell_holy_surgeoflight";
+$talentIcons["priest"][2][1][7] = "spell_holy_fanaticism";
+$talentIcons["priest"][2][1][9] = "spell_holy_impholyconcentration";
+$talentIcons["priest"][2][2][1] = "spell_holy_renew";
+$talentIcons["priest"][2][2][2] = "spell_holy_spellwarding";
+$talentIcons["priest"][2][2][3] = "spell_holy_blessedrecovery";
+$talentIcons["priest"][2][2][4] = "spell_holy_heal02";
+$talentIcons["priest"][2][2][5] = "inv_enchant_essenceeternallarge";
+$talentIcons["priest"][2][2][7] = "spell_holy_summonlightwell";
+$talentIcons["priest"][2][2][8] = "spell_holy_greaterheal";
+$talentIcons["priest"][2][2][9] = "spell_holy_circleofrenewal";
+$talentIcons["priest"][2][2][10] = "spell_holy_divineprovidence";
+$talentIcons["priest"][2][2][11] = "spell_holy_guardianspirit";
+$talentIcons["priest"][2][3][1] = "spell_holy_sealofsalvation";
+$talentIcons["priest"][2][3][2] = "spell_holy_sealofwrath";
+$talentIcons["priest"][2][3][4] = "spell_holy_searinglightpriest";
+$talentIcons["priest"][2][3][5] = "spell_holy_spiritualguidence";
+$talentIcons["priest"][2][3][6] = "spell_nature_moonglow";
+$talentIcons["priest"][2][3][7] = "spell_holy_blessedresillience";
+$talentIcons["priest"][2][3][8] = "spell_holy_serendipity";
+$talentIcons["priest"][2][3][9] = "spell_holy_testoffaith";
+$talentIcons["priest"][2][4][3] = "spell_holy_layonhands";
+
+$talentIcons["priest"][3][1][1] = "spell_shadow_requiem";
+$talentIcons["priest"][3][1][2] = "spell_shadow_shadowward";
+$talentIcons["priest"][3][1][3] = "spell_shadow_psychicscream";
+$talentIcons["priest"][3][1][5] = "spell_shadow_impphaseshift";
+$talentIcons["priest"][3][1][6] = "spell_shadow_skull";
+$talentIcons["priest"][3][1][8] = "spell_shadow_summonvoidwalker";
+$talentIcons["priest"][3][1][9] = "spell_shadow_psychichorrors";
+$talentIcons["priest"][3][2][1] = "spell_shadow_requiem";
+$talentIcons["priest"][3][2][2] = "spell_shadow_shadowwordpain";
+$talentIcons["priest"][3][2][3] = "spell_shadow_unholyfrenzy";
+$talentIcons["priest"][3][2][4] = "spell_magic_lesserinvisibilty";
+$talentIcons["priest"][3][2][5] = "spell_shadow_unsummonbuilding";
+$talentIcons["priest"][3][2][7] = "spell_shadow_shadowform";
+$talentIcons["priest"][3][2][9] = "spell_holy_stoicism";
+$talentIcons["priest"][3][2][11] = "spell_shadow_dispersion";
+$talentIcons["priest"][3][3][1] = "spell_shadow_gathershadows";
+$talentIcons["priest"][3][3][2] = "spell_shadow_burningspirit";
+$talentIcons["priest"][3][3][3] = "spell_shadow_siphonmana";
+$talentIcons["priest"][3][3][4] = "spell_shadow_chilltouch";
+$talentIcons["priest"][3][3][5] = "spell_shadow_improvedvampiricembrace";
+$talentIcons["priest"][3][3][6] = "spell_shadow_twilight";
+$talentIcons["priest"][3][3][7] = "spell_shadow_shadowpower";
+$talentIcons["priest"][3][3][8] = "spell_shadow_misery";
+$talentIcons["priest"][3][3][9] = "spell_shadow_painandsuffering";
+$talentIcons["priest"][3][3][10] = "spell_shadow_mindtwisting";
+$talentIcons["priest"][3][4][4] = "spell_shadow_blackplague";
+$talentIcons["priest"][3][4][5] = "spell_nature_focusedmind";
