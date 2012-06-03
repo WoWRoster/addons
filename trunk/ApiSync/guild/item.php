cc3eacc0208e3c5ec8b2f83d5800b07e0ed10d21
ulminia@gmail.com <ulminia@gmail.com@b904fcd1-60ed-da51-8dba-9922f2c0e645>
2012-06-03 17:14:17 +0000

    ApiSync - tpl updates shouldnt have any more broken div's

--- trunk/ApiSync/guild/item.php
+++ trunk/ApiSync/guild/item.php
@@ -1,37 +1,122 @@
 <?php
-	
-	//$char = $roster->api->Char->getCharInfo('Zangarmarsh','zonous','4');
-	$gemx = array();
-	$a = $roster->api->Data->getItemInfo('70138');
-	
+require_once (ROSTER_LIB . 'update.lib.php');
+		$update = new update();	
+echo ROSTER_PATH.'<br>';
+	$a = $roster->api->Data->getAchievInfo();
+	$rx = 0;
+	$rc = 0;
+foreach($a['achievements'] as $order => $cat)
+{
+	echo $cat['name'].' - '.$cat['id'].'<br>';
+	if (isset($cat['achievements']))
+	{
+		foreach($cat['achievements'] as $d => $achi)
+		{
+			//echo '-----'.$achi['title'].'<br>';
+			/*
+			[id] => 6
+			[title] => Level 10
+			[points] => 10
+			[description] => Reach level 10.
+			[rewardItems] => Array
+			[icon] => achievement_level_10
+			*/	
+			$tooltip = '<div style="width:100%;style="color:#FFB100""><span style="float:right;">' . $achi['points'] . ' Points</span>' . $achi['title'] . '</div><br>' . $achi['description'] . '';
+			$crit='';
+			$crit .= '<br><div class="meta-achievements"><ul>';
+			foreach ($achi['criteria'] as $r => $d)
+			{
+				$crit .= '<li>'.$d['description'].'</li>';
+				$update->reset_values();
+				$update->add_value('crit_achie_id',	$achi['id']);
+				$update->add_value('crit_id',		$d['id']);
+				$update->add_value('crit_desc',		$d['description']);
+				$querystr = "INSERT INTO `roster_addons_achievements_crit` SET " . $update->assignstr;
+				//$result = $roster->db->query($querystr);
+				//echo $querystr.'<br>';
+				$rc++;
+			}
+			$crit .= '</ul></div>';
+				
+			$tooltip .= $crit;
+				
+			$update->reset_values();
+			$update->add_value('achie_name',	$achi['title']);
+			$update->add_value('achie_desc',	$achi['description']);
+			$update->add_value('achie_points',	$achi['points']);
+			$update->add_value('achie_id',		$achi['id']);
+			$update->add_value('achie_icon',	$achi['icon']);
+			$update->add_value('achie_tooltip',	$tooltip);
+			$update->add_value('c_id',			$cat['id']);
+			$update->add_value('p_id',			'-1');
+			$update->add_value('achi_cate',		$cat['name']);
+			
+			$querystr = "INSERT INTO `roster_addons_achievements_achie` SET " . $update->assignstr;
+			//$result = $roster->db->query($querystr);
+			//echo$querystr.'<br>';
+			$rx++;
+			//*/
+		}
+	}
+	if (isset($cat['categories']))
+	{
+		foreach($cat['categories'] as $corder => $sub)
+		{
+			echo '--'.$sub['name'].' - '.$sub['id'].'<br>';
+			foreach($sub['achievements'] as $d => $achi)
+			{
+				//echo '-----'.$achi['title'].'<br>';
+				/*
+				[id] => 6
+				[title] => Level 10
+				[points] => 10
+				[description] => Reach level 10.
+				[rewardItems] => Array
+				[icon] => achievement_level_10
+				*/	
+				$tooltip = '<div style="width:100%;style="color:#FFB100""><span style="float:right;">' . $achi['points'] . ' Points</span>' . $achi['title'] . '</div><br>' . $achi['description'] . '';
+				$crit='';
+				$crit .= '<br><div class="meta-achievements"><ul>';
+				foreach ($achi['criteria'] as $r => $d)
+				{
+					$crit .= '<li>'.$d['description'].'</li>';
+					
+					$update->reset_values();
+					$update->add_value('crit_achie_id',	$achi['id']);
+					$update->add_value('crit_id',		$d['id']);
+					$update->add_value('crit_desc',		$d['description']);
+					$querystr = "INSERT INTO `roster_addons_achievements_crit` SET " . $update->assignstr;
+					//$result = $roster->db->query($querystr);
+					//echo $querystr.'<br>';
+					$rc++;
+				}
+				$crit .= '</ul></div>';
+					
+				$tooltip .= $crit;
+				
+				$update->reset_values();
+				$update->add_value('achie_name',	$achi['title']);
+				$update->add_value('achie_desc',	$achi['description']);
+				$update->add_value('achie_points',	$achi['points']);
+				$update->add_value('achie_id',		$achi['id']);
+				$update->add_value('achie_icon',	$achi['icon']);
+				$update->add_value('achie_tooltip',	$tooltip);
+				$update->add_value('c_id',			$sub['id']);
+				$update->add_value('p_id',			$cat['id']);
+				$update->add_value('achi_cate',		$sub['name']);
+				
+				$querystr = "INSERT INTO `roster_addons_achievements_achie` SET " . $update->assignstr;
+				//$result = $roster->db->query($querystr);
+				//echo $querystr.'<br>';
+				$rx++;
+				//*/
+			}
+		}
+	}
+}	
+echo '<br><hr><br> Achievements '.$rx.'<br>Criteria '.$rc.'<br>';
 	echo '<pre>';
 	print_r($a);
 	echo '</pre>';
 	echo '<br><hr><br>';
 	echo '<pre>';
-	/*
-	print_r($char['items']['shoulder']);
-	echo '</pre>';
-	$item = $char['items']['shoulder'];
-	if (isset($item['tooltipParams']['gem0']))
-	{
-		$gemx['gem0'] = $roster->api->Data->getItemInfo($item['tooltipParams']['gem0']);
-	}
-	if (isset($item['tooltipParams']['gem1']))
-	{
-		$gemx['gem1'] = $roster->api->Data->getItemInfo($item['tooltipParams']['gem1']);
-	}
-	if (isset($item['tooltipParams']['gem2']))
-	{
-		$gemx['gem2'] = $roster->api->Data->getItemInfo($item['tooltipParams']['gem2']);
-	}
-	echo '<br><hr><br>';
-	echo '<pre>';
-	print_r($gemx);
-	echo '</pre>';
-	*/
-	$tx =  $roster->api->Item->item($a,null,null);
-	echo $tx;
-	echo '<br><br><br>';
-	$e = $roster->api->Item->item($a,null,null);
-	echo $e;
